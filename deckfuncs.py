734fec822995196c8930556d4deff87d2f458938
Rocadinis <8b21espq@gmail.com>
2022-04-11 14:32:46 +0100

    Updated files
    Added optional function to shuffle decks, added 3 new functions, updated docs, added an example file

--- deckfuncs.py
+++ deckfuncs.py
@@ -1,23 +1,31 @@
 # Made by Rocadinis at github.com
 from random import shuffle
 def draw(deckVar, handVar, lenCheck):
-    if lenCheck == True and len(deckVar) <= 0:
+    if type(lenCheck) != bool:
+        raise TypeError("The lenCheck variable must be true or false")
+    elif lenCheck == True and len(deckVar) <= 0:
         pass
     else:
         drawn = deckVar.pop(0)
         handVar.append(drawn)
         return drawn
 
-def search(card, deckVar, handVar):
+def search(card, deckVar, handVar, shuffleAfter):
     deckVar.remove(card)
     handVar.append(card)
-    shuffle(deckVar)
+    if type(shuffleAfter) != bool:
+        raise TypeError("The shuffleAfter variable must be true or false")
+    elif shuffleAfter == True:
+        shuffle(deckVar)
     return card
 
-def bury(card, deckVar, gyVar):
+def bury(card, deckVar, gyVar, shuffleAfter):
     deckVar.remove(card)
     gyVar.append(card)
-    shuffle(deckVar)
+    if type(shuffleAfter) != bool:
+        raise TypeError("The shuffleAfter variable must be true or false")
+    elif shuffleAfter == True:
+        shuffle(deckVar)
     return card
 
 def mill(deckVar, gyVar, number):
@@ -28,8 +36,36 @@ def mill(deckVar, gyVar, number):
         milledList.append(topCard)
     return milledList
         
-def returnCard(card, deckVar, gyVar):
+def returnCard(card, deckVar, gyVar, shuffleAfter):
     gyVar.remove(card)
     deckVar.append(card)
-    shuffle(deckVar)
+    if type(shuffleAfter) != bool:
+        raise TypeError("The shuffleAfter variable must be true or false")
+    elif shuffleAfter == True:
+        shuffle(deckVar)
     return card
+
+def gyToHand(card, gyVar, handVar):
+    if len(gyVar) <= 0:
+        raise Exception("The graveyard / discard pile is empty")
+    else:
+        gyVar.remove(card)
+        handVar.append(card)
+        return card
+
+def topDeck(card, deckVar, gyVar):
+    if len(gyVar) <= 0:
+        raise Exception("The graveyard / discard pile is empty")
+    else:
+        gyVar.remove(card)
+        deckVar.insert(0, card)
+        return card
+
+def bottomDeck(card, deckVar, gyVar):
+    if len(gyVar) <= 0:
+        raise Exception("The graveyard / discard pile is empty")
+    else:
+        deckLength = len(deckVar)
+        gyVar.remove(card)
+        deckVar.insert(deckLength + 1, card)
+        return card
